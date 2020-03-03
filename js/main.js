2ad2667e926570ee4cf1ca4c196bdc2e68a4bea7
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2020-03-03 17:14:31 -0500

    added basic form

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function submitInformation() {
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    var email = document.getElementById('email').value;
+
+    alert("First name: " + firstName + "n Last name: " + lastName + "n Email: " + email);
+}
